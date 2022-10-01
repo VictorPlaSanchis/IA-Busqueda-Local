e19968c68f611471c2d71d39ed0a3588fe9f275f
mariamontfal <maria.montalvo@est.fib.upc.edu>
2022-10-01 20:44:59 +0200

    01/10/22: Solucion inicial -> case 0.

--- src/EnergiaBoard.java
+++ src/EnergiaBoard.java
@@ -1,4 +1,5 @@
 
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Vector;
@@ -33,7 +34,17 @@ public class EnergiaBoard {
     //ArrayList para almacenar los clientes
     private static Clientes clientes;
 
+    private static Clientes clientesG;
+
+    private static Clientes clientesNoG;
+
+    private static ArrayList<Double> energiaPendiente;
+
+    private static ArrayList<Integer> asignacionG;
+    private static ArrayList<Integer> asignacionNG;
+
     private Random random;
+
 
 
     /** Crea una nueva instancia de EnergiaBoard */
@@ -41,30 +52,76 @@ public class EnergiaBoard {
         centrales = (Centrales) new ArrayList<Central>();
         clientes = (Clientes) new ArrayList<Cliente>();
 
-        
+        //Separamos clientes garantizados de no garantizados
+        for (Cliente cliente : clientes) {
+            if (cliente.getTipo() == Cliente.NOGARANTIZADO) clientesNoG.add(cliente);
+            else clientesG.add(cliente);
+        }
 
+        nGarantizados = clientesG.size();
+        nNoGarantizados = clientesNoG.size();
+
+        nCentrales = centrales.size();
+        energiaPendiente = new ArrayList<Double>(nCentrales);
+
+        for(int i = 0; i<nCentrales; ++i){
+            energiaPendiente.set(i, centrales.get(i).getProduccion());
+        }
+        asignacionG = new ArrayList<Integer>(nGarantizados+nNoGarantizados);
     }
 
+    public void generarEstadoInicial(int opt){
+        switch (opt){
+            //Caso 0: colocar clientes en una central mientras la potencia remanente sea > 0
+            case 0:
+                int indexCentral = 0;
+                double energiaActualizada;
+                int i = 0;
+                while(i<nGarantizados && indexCentral<nCentrales){
+                    if(energiaPendiente.get(indexCentral)>=clientesG.get(i).getConsumo()){
+                        asignacionG.set(i,indexCentral);
+                        energiaActualizada = energiaPendiente.get(indexCentral) - clientesG.get(i).getConsumo();
+                        energiaPendiente.set(indexCentral, energiaActualizada);
+                        ++i;
+                    }
+                    else ++indexCentral;
+                }
+                if(indexCentral==nCentrales){
+                    while(i<nGarantizados){
+                        asignacionG.set(i,-1);
+                        ++i;
+                    }
+                    i = 0;
+                    while(i<nNoGarantizados){
+                        asignacionNG.set(i,-1);
+                    }
+                }
 
-    public EnergiaBoard(EnergiaBoard e) {
-        int i, j;
-        boolean aux[][]=e.getConnexions();
-        connexions = new boolean[aux.length][aux.length];
-        for (i=0; i<ncentrals+nrepetidors; i++)
-            for (j=0; j<ncentrals+nrepetidors; j++)
-                connexions[i][j]=aux[i][j];
+                else{
+                    i=0;
+                    while(i<nNoGarantizados && indexCentral<nCentrales){
+                        if(energiaPendiente.get(indexCentral)>=clientesNoG.get(i).getConsumo()){
+                            asignacionNG.set(i,indexCentral);
+                            energiaActualizada = energiaPendiente.get(indexCentral) - clientesNoG.get(i).getConsumo();
+                            energiaPendiente.set(indexCentral, energiaActualizada);
+                            ++i;
+                        }
+                        else ++indexCentral;
+                    }
 
-        int aux2[]=c.getGrau();
-        grau = new int[aux2.length];
+                    if(indexCentral==nCentrales){
+                        while(i<nNoGarantizados){
+                            asignacionNG.set(i,-1);
+                        }
+                    }
 
-        for (i=0; i<ncentrals+nrepetidors; i++)
-            grau[i]=aux2[i];
-
-        error_total = c.getErrorTotal();
-        num_repetidors = c.getNumRepetidors();
+                }
+            //Caso 1: Asignacion random de clientes a centrales
+            case 1:
 
 
-        myRandom = new Random();
+
+        }
     }
 
 }
